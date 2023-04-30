e96adb476ff5a78d5e50b264d1998647daafce99
DNS <glotov-aleks@mail.ru>
2023-04-30 14:54:37 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
 
-        System.out.println("Hello world!");
+    }
+
+    public static void task1() {
+        System.out.println("решение задачи №1");
+        int total = 0;
+        int deposit = 15000;
+        int i = 0;
+        while (total <= 2_459_000) {
+            if (total <= 2_459_000) {
+                i++;
+                total = total + deposit;
+                System.out.println("Месяц  " + i + ", сумма накоплений равна " + total + " рублей");
+            } else break;
+        }
     }
 }
