458db7477b934573fb2cc08c4dbdc0e1b2deceec
DNS <glotov-aleks@mail.ru>
2023-04-30 16:30:05 +0500

    task2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -29,6 +30,26 @@ public class Main {
         System.out.println();
         for (; i > 0; i--) {
             System.out.print(" " + i + " ");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Решение задачи №3");
+        int people = 12_000_000;
+        int fertility = 17;
+        int mortality = 8;
+        int result = 12_000_000 / 1000;
+        int fertilityResult = result * fertility;
+        int mortalityResult = result / mortality;
+        int earResult = people + fertilityResult - mortalityResult;
+        int i = 0;
+        int b = 0;
+        while (i < 10) {
+            if (b < 10) {
+                b++;
+                earResult++;
+                System.out.println("Год " + b + " ,численность населения составляет " + earResult);
+            } else break;
         }
     }
 }
